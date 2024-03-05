9418b7dd0d03abb7a64eca957381fdb29d92eed7
sakaisha <aishasakkary@outlook.com>
2024-03-05 14:00:47 -0800

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
+
 /**
- * bubble_sort - sort array from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - sort data by bubble
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ *
+ * Return:
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i = 0;
+	size_t j = 0;
+	size_t n = 1;
+	int temp = 0;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	{
+		for (j = 0; j < size - n; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
+
 		}
+		n++;
+	}
 }
